06f906bf4aa1441b2d4d7983c523d0af7004b5bf
jawade.arpeet <jawade.arpeet@gmail.com>
2026-04-08 20:54:12 +0530

    used viper to load config file. added config.json

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,28 +4,47 @@ import (
 	"fwd/internal/constants"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 type ServerConfig struct {
-	Port   string        `validate:"required,numeric"`
-	RunEnv constants.Env `validate:"required,oneof=dev stg prod"`
+	Port   string        `validate:"required,numeric" mapstructure:"port"`
+	RunEnv constants.Env `validate:"required,oneof=dev stg prod" mapstructure:"run_env"`
 }
 
 type Config struct {
-	Server *ServerConfig `validate:"required"`
+	Server *ServerConfig `validate:"required" mapstructure:"server"`
 }
 
 func LoadConfig() (*Config, error) {
-	cfg := &Config{
-		Server: &ServerConfig{
-			Port:   "8080",
-			RunEnv: constants.Dev,
-		},
-	}
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
 
-	if err := validator.New().Struct(cfg); err != nil {
+	if err := viper.ReadInConfig(); err != nil {
+		zap.L().Error("error while reading config file", zap.String("operation", "config.LoadConfig"), zap.Error(err))
 		return nil, err
 	}
 
-	return cfg, nil
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		zap.L().Error(
+			"error while unmarshaling config to struct",
+			zap.String("operation", "config.LoadConfig"),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	if err := validator.New().Struct(cfg); err != nil {
+		zap.L().Error(
+			"invalid config struct",
+			zap.String("operation", "config.LoadConfig"),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	return &cfg, nil
 }
