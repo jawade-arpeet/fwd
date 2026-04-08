a0f43774f1cdfbfd45676ff897a539ef69ebd0f0
jawade.arpeet <jawade.arpeet@gmail.com>
2026-04-09 00:16:08 +0530

    added accounts schema and postgres utils

---
+++ schemas/account_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS accounts (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
